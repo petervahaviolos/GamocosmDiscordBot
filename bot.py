a2aba57d388a2e78ea7b0437691f0752728621b6
petervahaviolos <vahaviolospeter@gmail.com>
2020-06-29 10:15:51 -0400

    First commit

---
+++ bot.py
@@ -0,0 +1,28 @@
+import datetime
+import sys
+from os.path import isdir
+from secrets import bot_token
+
+import discord
+from discord.ext import commands, timers
+from discord.utils import find, get
+
+client = commands.Bot(command_prefix='!')
+old_roles = {}
+
+
+@client.event
+async def on_ready():
+    print('Bot online')
+
+initial_extensions = ['cogs.server']
+
+if __name__ == '__main__':
+    for extension in initial_extensions:
+        try:
+            client.load_extension(extension)
+        except Exception as e:
+            print(f"Failed to load extension {extension}", file=sys.stderr)
+
+
+client.run(bot_token)
---
+++ secrets.py
@@ -0,0 +1,7 @@
+bot_token = 'NzI2OTQxOTkyMzIzNjQ1NTAw.XvkoSg.OHELBf6cKcoQgNq3ybbWIULezP0'
+login_url = 'https://gamocosm.com/users/sign_in'
+payload = {
+    "username": "",
+    "password": "",
+    "AUTH_TOKEN": ""
+}
